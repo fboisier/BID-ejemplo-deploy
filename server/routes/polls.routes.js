9d38df7d11e7f05948d06969f2ca91042d9a3b0d
Francisco Boisier <fboisier@codingdojo>
2023-02-20 22:14:40 -0300

    se actualiza proyecto

--- server/routes/polls.routes.js
+++ server/routes/polls.routes.js
@@ -1,10 +1,10 @@
 const PollsController = require('../controllers/polls.controller');
 module.exports = function (app) {
-    app.post('/polls/new', PollsController.createPolls);
-    app.get('/polls', PollsController.getAllPolls);
-    app.get('/polls/top', PollsController.getTop);
-    app.get('/polls/:id', PollsController.getPolls);
-    app.put('/polls/:id', PollsController.updatePolls);
+    app.post('/api/polls/new', PollsController.createPolls);
+    app.get('/api/polls', PollsController.getAllPolls);
+    app.get('/api/polls/top', PollsController.getTop);
+    app.get('/api/polls/:id', PollsController.getPolls);
+    app.put('/api/polls/:id', PollsController.updatePolls);
     
     // app.delete('/polls/:id', PollsController.deletePolls);
 }
